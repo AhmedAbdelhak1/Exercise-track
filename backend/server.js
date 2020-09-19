da1e0dfa7624b5eac404318244551595958f59cd
AhmedAbdelhak1 <ahmedabdelhak0@gmail.com>
2020-09-19 14:38:46 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useCreateIndex', true);
+mongoose.connect(uri,{ useNewUrlParser: true});
+
+
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongodb database connect successfully");
+})
+
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(port, () => {
+    console.log(`server runing on port: ${port}`)
+});
